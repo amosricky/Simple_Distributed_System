0b72f377578b731dd4f631d68efa9af574704e6d
ricky <amosricky95@gmail.com>
2020-01-14 00:38:06 +0800

    Loop interactive terminal

--- slave/slaveNode.go
+++ slave/slaveNode.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"Simple_Distributed_System/slave/cmd"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -50,5 +54,34 @@ func main() {
 	//	logrus.Printf("Reply [PostNewGame]：%s", resultJson)
 	//}
 
-	cmd.Execute()
+	fmt.Println("$ You could use [help] to get some instruction or [exit] to leave the terminal.")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("$ ")
+		cmdString, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		cmdString = strings.TrimSuffix(cmdString, "\n")
+		arrCommandStr := strings.Fields(cmdString)
+
+		newOsArgs := []string{os.Args[0]}
+		newOsArgs = append(newOsArgs, arrCommandStr...)
+		os.Args = newOsArgs
+
+		if len(os.Args) == 1{
+			continue
+		}
+
+		switch os.Args[1] {
+		case "help":
+			os.Args = []string{os.Args[0], "-h"}
+			cmd.Execute()
+		case "exit":
+			os.Exit(1)
+		default:
+			cmd.Execute()
+		}
+	}
 }
