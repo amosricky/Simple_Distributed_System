5b612b8b69bcdea9192dd4b26b197567494ae74a
Ricky Hsu <rickyhsu@qnap.com>
2020-01-14 16:47:44 +0800

    Finish CLI

--- slave/cmd/cmd.go
+++ slave/cmd/cmd.go
@@ -1,25 +1,30 @@
 package cmd
 
 import (
+	"Simple_Distributed_System/pb"
+	"Simple_Distributed_System/setting"
+	"encoding/json"
 	"fmt"
-	"os"
-
-	homedir "github.com/mitchellh/go-homedir"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"os"
 )
-
-var cfgFile string
+var conn *grpc.ClientConn
+var err error
 var gameID string
 var gameName string
 var gameTeam int32
 var gameRound int32
 var gameAdd int32
 
-// RootCmd represents the base command when called without any subcommands
-var RootCmd = &cobra.Command{Use: "",}
+var rootCmd = &cobra.Command{Use: "",}
 
-var gameCmd = &cobra.Command{Use: "game",}
+var gameCmd = &cobra.Command{
+	Use: "game",
+	Short: "Get & Modify game record",
+	Long: `Get & Modify game record`,}
 
 var scoreCmd = &cobra.Command{
 	Use: "score",
@@ -27,9 +32,16 @@ var scoreCmd = &cobra.Command{
 	Long: `Get score by game ID.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(gameID)!=0{
-			fmt.Printf("MY gameID  %s",gameID)
+			connGetScore := pb.NewServiceServerClient(conn)
+			resultGetScore, err := connGetScore.GetScore(context.Background(), &pb.GetScoreRequest{ID:gameID})
+			if err != nil {
+				logrus.Warnf("Can't execute [GetScore] function：%v", err.Error())
+			}else {
+				resultJson, _ := json.Marshal(resultGetScore)
+				logrus.Printf("Reply [GetScore]：%s", resultJson)
+			}
 		}else {
-			fmt.Println("scoreCmd")
+			logrus.Printf("Incomplete command")
 		}
 	},
 }
@@ -38,9 +50,15 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Get game list. (Contain gameName & gameID)",
 	Long: `Get game list. (Contain gameName & gameID)`,
-	//Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("listCmd")
+		connGetGameList := pb.NewServiceServerClient(conn)
+		resultGetGameList, err := connGetGameList.GetGameList(context.Background(), &pb.GeneralRequest{})
+		if err != nil {
+			logrus.Warnf("Can't execute [GetGameList] function：%v", err.Error())
+		}else {
+			resultJson, _ := json.Marshal(resultGetGameList)
+			logrus.Printf("Reply [GetGameList]：%s", resultJson)
+		}
 	},
 }
 
@@ -48,19 +66,20 @@ var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Add point by game ID.",
 	Long: `Add point by game ID.`,
-	//Args: cobra.MinimumNArgs(4),
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(gameID)!=0{
-			fmt.Printf("MY gameID %v",gameID)
-		}
-		if gameTeam!=0{
-			fmt.Printf("MY gameTeam %v",gameTeam)
-		}
-		if gameRound!=0{
-			fmt.Printf("MY gameRound %v",gameRound)
-		}
-		if gameAdd!=0{
-			fmt.Printf("MY gameAdd %v",gameAdd)
+		if len(gameID)!=0 && gameTeam!=-1 && gameRound!=0 && gameAdd!=0{
+			connPutScore := pb.NewServiceServerClient(conn)
+			gameTeamChangeType := pb.PutScoreRequest_TeamType(gameTeam)
+
+			resultPutScore, err := connPutScore.PutScore(context.Background(), &pb.PutScoreRequest{ID:gameID, Team:gameTeamChangeType, Round:gameRound, Add:gameAdd})
+			if err != nil {
+				logrus.Warnf("Can't execute [PutScore] function：%v", err.Error())
+			}else {
+				resultJson, _ := json.Marshal(resultPutScore)
+				logrus.Printf("Reply [PutScore]：%s", resultJson)
+			}
+		} else {
+			logrus.Printf("Incomplete command")
 		}
 	},
 }
@@ -69,10 +88,18 @@ var newGameCmd = &cobra.Command{
 	Use:   "new",
 	Short: "Create a new game.",
 	Long: `Create a new game.`,
-	//Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(gameName)!=0{
-			fmt.Printf("MY gameName %v",gameName)
+			connPostNewGame := pb.NewServiceServerClient(conn)
+			resultPostNewGame, err := connPostNewGame.PostNewGame(context.Background(), &pb.PostNewGameRequest{Game:gameName})
+			if err != nil {
+				logrus.Warnf("Can't execute [PostNewGame] function：%v", err.Error())
+			}else {
+				resultJson, _ := json.Marshal(resultPostNewGame)
+				logrus.Printf("Reply [PostNewGame]：%s", resultJson)
+			}
+		}else {
+			logrus.Printf("Incomplete command")
 		}
 	},
 }
@@ -80,49 +107,33 @@ var newGameCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-
-	if err := RootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	if err := rootCmd.Execute(); err != nil {
+		logrus.Printf("Can't execute salve node：%v", err.Error())
 	}
+}
+
+func Exit()  {
+	defer conn.Close()
+	os.Exit(1)
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.TempProject.yaml)")
+	// Init Cli
+	cobra.OnInitialize()
 	scoreCmd.Flags().StringVarP(&gameID, "id", "i", "", "game id")
 	addCmd.Flags().StringVarP(&gameID, "id", "i", "", "game id")
-	addCmd.Flags().Int32VarP(&gameTeam, "team", "t", 0, "team ([1]home [2]visitor)")
+	addCmd.Flags().Int32VarP(&gameTeam, "team", "t", -1, "team ([0]home [1]visitor)")
 	addCmd.Flags().Int32VarP(&gameRound, "round", "r", 0, "round ([min]1 [max]9)")
 	addCmd.Flags().Int32VarP(&gameAdd, "add", "a", 0, "add point")
 	newGameCmd.Flags().StringVarP(&gameName, "name", "n", "", "game name")
-	RootCmd.AddCommand(gameCmd)
+	rootCmd.AddCommand(gameCmd)
 	gameCmd.AddCommand(scoreCmd, listCmd, addCmd, newGameCmd)
-}
 
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".TempProject" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".TempProject")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	//// Init DB connection
+	setting.Setup()
+	serverUrl := fmt.Sprintf("localhost:%v", setting.ServerSetting.Port)
+	conn, err = grpc.Dial(serverUrl, grpc.WithInsecure())
+	if err != nil {
+		logrus.Warnf("Can't connect to server：%v", err.Error())
 	}
 }
